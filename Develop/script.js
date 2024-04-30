37380ae70c8dcd60c04a122f13e0b761a7f37df2
Nathan Lane <lanenathan214@gmail.com>
2024-04-30 15:22:14 -0400

    wrote function to handle saving and displaying the different time intervals based on user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 
 $(document).ready(function(){
+  $('.saveBtn').on('click', function (){
+    var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
 
+    localStorage.setItem(time,value);
+
+    $('.notification').addClass('show');
+
+    setTimeout(function (){
+      $('.notification').removeClass('show');
+    },5000);
+  });
 
 
 
